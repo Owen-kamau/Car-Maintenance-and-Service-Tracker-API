750d1dd558860c671cb0d0a12ed6f1f89c507274
David Rodgers <167431742+Davidrodges@users.noreply.github.com>
2025-11-11 18:42:16 +0300

    Change email to se output but return original  test_mail.php

--- test_mail.php
+++ test_mail.php
@@ -1,7 +1,7 @@
 <?php
 require 'mail.php'; // uses your existing mail.php file
 
-$to = "David.Ochieng@strathmore.edu"; // change this to your email
+$to = "owen.kamau@strathmore.edu"; // change this to your email
 $subject = "✅ Test Email from CMTS";
 $body = "
 <h2>Hi there!</h2>
