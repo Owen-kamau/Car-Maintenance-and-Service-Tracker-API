c0c0dde452d3c1f40e59f321c67ffa7ea864b888
Owen-kamau <167431522+Owen-kamau@users.noreply.github.com>
2026-06-05 22:52:45 +0300

    Login page

--- src/login.php
+++ src/login.php
@@ -167,10 +167,10 @@ a:hover {
 
         <form method="post">
             <label>Email:</label><br>
-            <input type="email" name="email" required><br><br>
+            <input type="email" name="email" autocomplete="email" required><br><br>
 
             <label>Password:</label><br>
-            <input type="password" name="password" required><br><br>
+            <input type="password" name="password" autocomplete="current-password" required><br><br>
 
             <button type="submit">Login</button>
         </form>
